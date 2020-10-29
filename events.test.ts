682b8eecbafb36ac909cc7988b80f8742c65bc8a
AlonBru <indyalon@gmail.com>
2020-10-30 01:40:36 +0200

    should work now
    hopefully

--- events.test.ts
+++ events.test.ts
@@ -1,9 +1,11 @@
 import { mockData } from "./mock_data";
 import request from "supertest";
-import { Event as event } from "../analytics-challenge/client/src/models/event"
-const app = require('../analytics-challenge/server/backend/app');
-// import { Event as event } from "../_cloned-app/client/src/models/event"
-// const app = require('../_cloned-app/server/backend/app');
+// import app  from '../analytics-challenge/server/backend/app';
+// import { Event as event } from "../analytics-challenge/client/src/models/event"
+// import db from '../analytics-challenge/server/backend/database'
+import app  from '../_cloned-app/server/backend/app';
+import { Event as event } from "../_cloned-app/client/src/models/event"
+import db from '../_cloned-app/server/backend/database'
 
 const isEvent = (event: any ): event is event => {
   if(
@@ -29,62 +31,90 @@ const isEventArray = (events: any[]): events is event[] => {
 
 describe("main test", () => {
 
- it("can get all events", async () => {
-   const { body: allEvents } = await request(app).get("/events/all").expect(200);
-   expect(allEvents.length).toBe(50);
- });
+  beforeAll(async (done) => {
+    db.setState(mockData).write()
+    done()
+  })
+
+  it("can get all events", async () => {
+    const { body: allEvents } = await request(app).get("/events/all").expect(200);
+    expect(allEvents.length).toBe(250);
+  });
 
   it("getting all events from the server must return array of event types", async () => {
-   const { body: allEvents } = await request(app).get("/events/all").expect(200);
-   expect(isEventArray(allEvents)).toBe(true);
- });
+    const { body: allEvents } = await request(app).get("/events/all").expect(200);
+    expect(isEventArray(allEvents)).toBe(true);
+  });
 
- it("can post new event", async () => {
-   await request(app).post("/events").send(mockData.events[0]).expect(200);
-   const { body: allEvents2 } = await request(app).get("/events/all").expect(200);
-   expect(allEvents2.length).toBe(51);
-   expect(allEvents2[50].date).toBe(mockData.events[0].date);
-   expect(allEvents2[50].os).toBe(mockData.events[0].os);
- });
+  it("can post new event", async () => {
+    await request(app).post("/events").send(mockData.events[0]).expect(200);
+    const { body: allEvents2 } = await request(app).get("/events/all").expect(200);
+    expect(allEvents2.length).toBe(251);
+    expect(allEvents2[250].date).toBe(mockData.events[0].date);
+    expect(allEvents2[250].os).toBe(mockData.events[0].os);
+  });
 
- it("can get unique sessions count by day", async () => {
-   const { body: sessionsByDays } = await request(app).get("/events/by-days/0").expect(200)
+  it("can get unique sessions count by day", async () => {
+    const { body: sessionsByDays } = await request(app).get("/events/by-days/0").expect(200)
 
-   expect(sessionsByDays.length).toBe(7)
-   expect(sessionsByDays.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(47)
-   expect(sessionsByDays[0].count).toBe(6);
+    expect(sessionsByDays.length).toBe(7)
+    expect(sessionsByDays.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(48)
+    expect(sessionsByDays[0].count).toBe(7);
 
-   const { body: sessionsByDays2 } = await request(app).get("/events/by-days/1").expect(200)
+    const { body: sessionsByDays2 } = await request(app).get("/events/by-days/7").expect(200)
 
-   expect(sessionsByDays2.length).toBe(7)
-   expect(sessionsByDays2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(44)
-   expect(sessionsByDays2[0].count).toBe(0);
-   expect(sessionsByDays2[1].count).toBe(6);
-   expect(sessionsByDays2[6].count).toBe(8);
- });
+    expect(sessionsByDays2.length).toBe(7)
+    expect(sessionsByDays2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(52)
+    expect(sessionsByDays2[0].count).toBe(7);
+    expect(sessionsByDays2[1].count).toBe(8);
+    expect(sessionsByDays2[6].count).toBe(7);
+  });
 
- it("can get unique sessions count by hour", async () => {
-   const { body: sessionsByHours } = await request(app).get("/events/by-hours/48").expect(200)
 
-   expect(sessionsByHours.length).toBe(24)
-   expect(sessionsByHours.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(8)
+  
 
-   const { body: sessionsByHours2 } = await request(app).get("/events/by-hours/36").expect(200)
+  it("can get unique sessions count by hour", async () => {
+    const { body: sessionsByHours } = await request(app).get("/events/by-hours/0").expect(200)
 
-   expect(sessionsByHours2.length).toBe(24)
-   expect(sessionsByHours2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(7)
- });
+    expect(sessionsByHours.length).toBe(24)
+    expect(sessionsByHours.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(6)
 
- const charts: string[] = ['os','pageview','geolocation']
- const timeRange: string[] = ['today','week','all']
- 
- charts.forEach(chart => {
-   timeRange.forEach(timeRange => {
-     it(`${chart} entry point can get ${timeRange} as a param to determine the wanted time range for the data`, async () => {
-       const { body: events } = await request(app).get(`/events/chart/${chart}/${timeRange}`).expect(200);
-       expect(events.length).not.toBe(0);
-     }); 
-   });
- });
- 
+    const { body: sessionsByHours2 } = await request(app).get("/events/by-hours/1").expect(200)
+
+    expect(sessionsByHours2.length).toBe(24)
+    expect(sessionsByHours2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(8)
+  });
+  
+  //deleted timeoneurl tests
+  const charts: string[] = ['os','pageview','geolocation']
+  const timeRange: string[] = ['today','week','all']
+  
+  charts.forEach(chart => {
+    timeRange.forEach(timeRange => {
+      it(`${chart} entry point can get ${timeRange} as a param to determine the wanted time range for the data`, async () => {
+        const { body: events } = await request(app).get(`/events/chart/${chart}/${timeRange}`).expect(200);
+        expect(events.length).not.toBe(0);
+      }); 
+    });
+  });
+  
+  // it("can get time of each URL for 'time on URL chart'", async () => {
+  //   const { body: time } = await request(app).get("/chart/timeonurl/allusers").expect(200)
+  //   const { body: timeIHours } = await request(app).get("/chart/timeonurl/inhours").expect(200)
+  //   expect(time.length).toBe(7)
+  //   expect(countBy((day) => day.count, time)).toBe(47)
+  // });
+
+  it("retention cohort", async () => {
+
+    const { body: retentionData } = await request(app).get("/events/retention").expect(200);
+    expect(retentionData.length).toBe(6);
+
+    expect(retentionData[0].weeklyRetention.slice(0,3)).toEqual([100, 27, 18]);
+    expect(retentionData[1].weeklyRetention.slice(0,2)).toEqual([100, 57]);
+    expect(retentionData[2].weeklyRetention[0]).toBe(100);
+
+    expect(retentionData[1].weeklyRetention[4]).toBe(0);
+
+  });
 })
